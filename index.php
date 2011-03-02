de286f13a4a77fd6e8d187f9e28798c1162118f4
Andrew Sommerfeld <andrews@criticalmass.com>
2011-03-02 11:29:56 -0600

    main assassin lipsum generator

---
+++ index.php
@@ -0,0 +1,181 @@
+<?php
+$sentences = array(1 =>
+    "100%. You know, here's your first pee test. Next one goes in your mouth.",
+    "No, you won't get high .... It's all good. It's all good, guys. Quit panicking. Quit panicking. No panic, no judgment.",
+
+    "The goddesses? Alex, the goddesses .... Let me just say this about the goddesses: I don't believe the term is good enough ... is good enough ... but when you're bound by these terrestrial descriptions, you must use the best choice available, right?",
+
+    "So if you think about it, dude, it's like I'm 0-for-3 with marriage, with ne'er an excuse.",
+    "But like in baseball, the scoreboard doesn't lie. Never has. So what we all have is a marriage of the heart ... of the hearts.",
+    "And you know, to sully and to contaminate or radically disrespect this union with a shameful contract is something I'll leave to the amateurs and the bible grippers.",
+
+    "THE BIBLE GRIPPERS!",
+    ".... And then, I just got to add this: There was a whole firestorm yesterday about Brooke being, you know, a part of our crew. And let me just say this ... this is all I'm gonna say about it: Where there were four ... there are now three. Goodbye, Brooke, and good luck in your travels ... you're gonna need it ... badly... So ....",
+
+    "No. She's not there now.",
+    "And we are and, I don't know, winning?... Anyone? .... (long silence) .... Rhymes with winning? .... Anyone? .... Yeah, that would be us.",
+
+    "Well, I think it was Nails that said I was really, really flattered, 'cause he got it right. And I'm not ...",
+    "You know, he might be Nails, but I'm frickin' bayonets, you know? I am battle-tested bayonets, bro.",
+    "And you know, I'm tired ... I'm so tired of pretending like my life isn't perfect and bitchin' and just winning every second .... And I'm not perfect and bitchin', and just delivering the goods at every frickin' turn.",
+    "Because ... look at what I'm dealing with, man. I'm dealing with fools and trolls.",
+    "You know? Dealing with soft targets, and it's just strafing runs in my underwear before my first cup of coffee because I don't have time for these clowns. I don't have time for their judgment and their stupidity and .... (sighs)",
+    "You know, they lay down with their ugly wives in front of their ugly children and just look at their loser lives and then they look at me and say, 'I CAN'T PROCESS IT!' Well, no, and you never will. Stop trying. Just sit back and enjoy the show .... You know?",
+
+    "'You have the right to kill me, but you do not have the right to judge me.' Boom. That's the whole movie. That's life .... That's life. There's nobility in that. There's focus. It's genuine. It's crystal and it's pure and it's available to everybody.",
+
+    "Yeah, I didn't care about that vanity card. In fact, I went straight on with that one and just dispelled that one.",
+    "And that was actually, you know, one of the few compliments that clown has paid me in frickin' almost a decade.",
+    "Um ... but I'm excited to get back ... to work. Um ... and not to completely discount what you just talked about ... It's just that if I bring up these ... these turds ... these little losers ... there's no reason to then bring them back into the fold, because I have real fame, and they have nothing.",
+    "They have zero. They have that night, and I will forget about them as the last image of them exits my beautiful home. And they will get out there and they will sell me, and they will lose.",
+    "They will lose the rest of their lives as they think about me and my life for the rest of their lives. So ... it's ... there's no ... again, bring me a challenge, somebody. Bring me a frickin' challenge. Because, you know ... it just ain't there. WINNING!",
+
+    "Well, yeah. But I was gonna tell you, I'm tired of being told like, 'Oh, you can't talk about that. You can't talk about that.' Bull s.h.i.t. ... uh ....",
+    "Let me just say this: It's nothing this side of deplorable, that a certain Chaim Levine - yeah, that's Chuck's real name - mistook this rock star for his own selfish exit strategy, bro.",
+    "Check it, Alex, I embarrassed him in front of his children and the world by healing at a pace that his unevolved mind can't process. Okay ... last I checked, Chaim, I've spent close to the last decade, I don't know, effortless and magically converting your tin cans into pure gold.",
+    "And the gratitude I get is this charlatan chose not to do his job, which is to WRITE. Clearly, someone who believes he is above the law. Well, you've been warned, dude. Bring it.",
+
+    "It's ... yeah ... it's an understatement. You know, I just ... I'm sorry, man, I got magic and I got poetry in my fingertips.",
+    "And I'm ... you know ... most of the time. And this includes naps! You know, I'm an F-18, bro. And I will destroy you in the air. And I will deploy my ordinance to the ground. And, uh ....",
+
+    "Yeah, why not? 'Cause it's just pure ... pure and complete gnarlyisms.",
+    "Yeah, I sat with two - hey, coincidence - F-18, Top Gun, radical fire, napalm dropping pilots in my movie theater watching the chopper attack sequence on the beachhead to go surfing because they wanted to and those people were in their way.",
+    "And I was getting a tattoo during the death from above. And it's the banner from the death card that Kilgore is throwing on his victims.",
+    "But there's also ... falling from it ... is the apple from The Giving Tree. There's my life. Deal with it. Oh, wait! Can't process it. Losers! WINNING! B'bye.",
+
+    "Major League 3? I don't know. I haven't read it. I don't care.",
+    "It's being directed and written by a genius name David Ward who - oh, I don't know - won the Academy Award at 23 for writing The Sting. And it was his pen and his vision that created the classic that we know today as Major League.",
+    "In fact, a lot of people think the movie's called 'Wild Thing', as they should.",
+    "But ... whatever. I agreed to do it. There's just one deal point that Jim Robinson - a wonderful man at Morgan Creek, a great company - need to work out is they want me in it. If they do, it's a smash. If they don't, it's a turd that opens on a tugboat.",
+
+    "Because I'm just ... um .... I don't know. Why give an interview when you can leave a warning, you know?",
+    "But it's just not ... I'm not fair game. I'm not a soft target. It's over.",
+    "There's a new sheriff in town, and he has an army of assassins. Oh, we must speak of the Vatican assassins.",
+    "Yes! I don't read anything after it's come out, because I will not waste my precious time on such ridiculous nonsense. But I understand, according to you, that there was a lot of curiosity about the Vatican assassins.",
+    "And I'm like ... guys, it's right there in the thing. Duh! We work for the Pope. We murder people. We're Vatican assassins.",
+    "How complicated can it be? What they're not ready for, is guys like you and I, Nails, and all the other gnarly gnarlingtons in my life ... that we are high priests ... Vatican assassin warlocks. Boom. Print that, people. See where that goes.",
+
+    "Yeah. I did. Because he's nothing shy of that. And it is his job to go out there and embarrass people. Embarrass them. Not just beat them, but embarrass them in the process.",
+    "He's as radical as you'd think he'd might be. If ... I'm not just my dad, I'm ... you know ... petting up the river to kill another part of me, which is 'courage'.",
+    "I am every character in between, save for that little weirdo with his guts strapped in, begging for water. That's not me.",
+    "But there are parts of me that is hopper, because my motto now is 'You either love, or you hate ... and you must do so violently.' And the reason you must hate violently is because ... and you have to hate everyone that is not in your family, because they're there to destroy your family, and they will come at you in all forms and shapes.",
+    "And therefore there's nothing in the middle. I don't live in the middle anymore. That's where you get slaughtered. That's where you get embarrassed in front of the prom queen. And it's just not an option.",
+    "So ... you know ... within that is tremendous focus, and tremendous clarity, and tremendous peace.",
+    "Because you have absolute and total resolve about your decisions. If you love with violence and you hate with violence, there's nothing that can be questioned.",
+    "And people say, 'Oh, you know... you have to work through your resentments.' Yeah. No. I'm gonna hold on to them and they're gonna fuel my attack.",
+    "And they're gonna fuel the battle-cry of my deadly and dangerous secret and silent soldiers. 'Cause they're all around you .... Anyway. Thought you were just messing with one dude. Sorry! WINNING!",
+
+    "Yeah, but I can't use the word 'sober' because that's a term from 'those people', and I have cleansed myself.",
+    "I just closed my eyes and in a nanosecond I cured myself from this ridiculous model of disease, addiction, and obsession. It's just ... It's just the work of sissies.",
+    "The only thing I'm addicted to right now is winning. You know? This bootleg cult - you know, arrogantly referred to as 'Alcoholics Anonymous' - now supports a 5% success rate. My success rate is 100%. Do the math!",
+    "Take for instance its founder and death written ... I don't know ... broken down plagiarist ... stayed high on acid until the day he died. I can't seem to find that chapter in his silly book of lies.",
+    "And Alex, try to get your mind around this, as a fellow warrior, deep in the trenches: Their entire manifesto is built upon complete and total surrender ... or the concept of complete and total surrender.",
+    "They urge you to put down your sword and come join the winners. In 22 years the only 'winners' I could locate in their toothless warren were either driving a convertible van or living like trolls under an abandoned bridge.",
+    "And one of those stupid mottoes, Alex, is 'Don't be special. Be one of us.' News flash! I am special, and I'll never be one of you! ... There it is.",
+
+    "Well, I'm not Thomas Jefferson .... He was a pussy!",
+    "But I dare anyone to debate me on things.",
+    "Debate me on AA right now. I have a disease? Bullshit. I cured it with my brain; with my mind.",
+    "I'm cured. I'm done. 'Well, we all have to sit in here and touch ourselves and frown.' Well, you don't look like you're having a lot of fun.",
+    "I'm gonna hang out with these two smokin' hotties and fly privately around the world. You know, it might be lonely up here, but I sure like the view, Alex.",
+
+    "I'm done. I'm done. It's on. Bring it, as I say. I ain't hidin'.",
+    "And if you're a part of my family, I will love you violently. If you infiltrate and try to hurt my family, I will murder you violently."
+);
+
+$nbr = (isset($_GET['nbr']) and 
+         in_array($_GET['nbr'], range(1,count($sentences)))) ?
+         $_GET['nbr'] : 1;
+$elem = (isset($_GET['elem']) and
+         in_array($_GET['elem'], array('p', 'li', 'div','none'))) ?
+         $_GET['elem'] : 'p';
+$sent = (isset($_GET['sent']) and 
+         in_array($_GET['sent'], range(1,count($sentences)))) ?
+         $_GET['sent'] : 1;
+?>
+<form action='<?php echo $_SERVER['PHP_SELF']; ?>' method='get'>
+<fieldset>
+<legend>Lorem Ipsum Generator</legend>
+<p>
+<label>HTML element type:
+<select name='elem'>
+<?php
+$elements = array(
+   'p' => 'paragraph',
+   'li' => 'list item',
+   'div' => 'div',
+   'none' => 'none'
+);
+foreach($elements as $key => $val)
+{
+   echo "<option value='$key'";
+   if(isset($_GET['elem']) and $_GET['elem'] == $key)
+   {
+      echo " selected='selected'";
+   }
+   echo ">$val</option>\n";
+}
+?>
+</select>
+</label>
+</p>
+<p>
+<label>Number of elements:
+<select name='nbr'>
+<?php
+for($ix = 1; $ix <= count($sentences); $ix++)
+{
+   echo "<option value='$ix'";
+   if(isset($_GET['nbr']) and $_GET['nbr'] == $ix)
+   {
+      echo " selected='selected'";
+   }
+   echo ">$ix</option>\n";
+}
+?>
+</select>
+</label>
+</p>
+<p>
+<label>Number of sentences per element:
+<select name='sent'>
+<?php for($ix = 1; $ix <= count($sentences); $ix++)
+{
+   echo "<option value='$ix'";
+   if(isset($_GET['sent']) and $_GET['sent'] == $ix)
+   {
+      echo " selected='selected'";
+   }
+   echo ">$ix</option>\n";
+}
+?>
+</select>
+</label>
+</p>
+<p><input type='submit' value='Get Lorem Ipsum text'></p>
+</fieldset>
+</form>
+<pre id='copytext'>
+<?php
+$count = 1;
+$text = array();
+for($i = 1; $i <= $nbr; $i++)
+{
+   if($elem !== "none"){
+       $text[$i] = "<$elem>";
+   }
+   for($j = 1; $j <= $sent; $j++)
+   {
+       $rand = rand(1,count($sentences));
+      $text[$i] .= $sentences[$rand].' ';
+   }
+   if($elem !== "none"){
+   $text[$i] = trim($text[$i])."</$elem>";
+   }else{
+       $text[$i] = trim($text[$i]);
+   }
+   $text[$i] = wordwrap($text[$i]);
+}
+echo htmlspecialchars(implode("\n\n", $text));
+?>
+</pre>
